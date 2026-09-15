1a1408c4dc0abc422928eaf71d850565538f1b09
zoerilla-netizen <wilmanefortullien1@gmail.com>
2026-09-15 01:12:40 -0400

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited. It is regenerated automatically
-// by `next dev` and `next build` the first time you run them.
